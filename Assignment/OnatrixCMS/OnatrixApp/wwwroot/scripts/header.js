6ffd60b50f867bb981d89e4dfafc984226f80b51
Dyssling <olle.lundgarden@hotmail.com>
2024-09-11 21:15:51 +0200

    Home page and sub pages created, along with static header and footer.

---
+++ Assignment/OnatrixCMS/OnatrixApp/wwwroot/scripts/header.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    if (document.getElementById("nav").style.display != "block"){
+        document.getElementById("nav").style.display = "block";
+    }
+
+    else{
+        document.getElementById("nav").style.display = "none";
+    }
+}
