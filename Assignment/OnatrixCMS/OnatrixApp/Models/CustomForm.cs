f090b215bcd1f1373b95b92e7490cdacf456cc6c
Dyssling <olle.lundgarden@hotmail.com>
2024-09-16 20:37:20 +0200

    Form component created and added to page. TODO: Surface controller and validation

---
+++ Assignment/OnatrixCMS/OnatrixApp/Models/CustomForm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnatrixApp.Models
+{
+	public class CustomForm
+	{
+		[DisplayFormat(ConvertEmptyStringToNull = false)]
+		public string? FullName { get; set; } = null!;
+
+		[DisplayFormat(ConvertEmptyStringToNull = false)]
+		[RegularExpression("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,})+$")]
+		public string? Email { get; set; } = null!;
+
+		[DisplayFormat(ConvertEmptyStringToNull = false)]
+		[RegularExpression("^[0-9*#+ .()-]{7,}$")]
+		public string? Phone { get; set; } = null!;
+
+		[DisplayFormat(ConvertEmptyStringToNull = false)]
+		public string? Service { get; set; } = null!;
+
+		[DisplayFormat(ConvertEmptyStringToNull = false)]
+		public string? Message { get; set; } = null!;
+
+	}
+}
